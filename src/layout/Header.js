c5410ea38e73aabf3db7013dd8daa795efac700c
ihabyoussef <902519@mof.gov.sa>
2021-10-13 11:40:45 +0200

    feature/ css enhancment

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
 
     return (
       <header className="header">
-        <h1>Gallery</h1>
+        <h1 className="logo-text">Poolia Gallery</h1>
       </header>
     );
      
